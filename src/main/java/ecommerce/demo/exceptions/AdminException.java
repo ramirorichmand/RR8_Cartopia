f3774046fbf42987d95b7d040c1b931c08d97b14
Ramiro <rrcolumbia21@gmail.com>
2023-05-14 11:48:28 +0100

    admin exception

--- src/main/java/ecommerce/demo/exceptions/AdminException.java
+++ src/main/java/ecommerce/demo/exceptions/AdminException.java
@@ -1,4 +1,15 @@
 package ecommerce.demo.exceptions;
 
-public class AdminException {
+public class AdminException extends Exception {
+
+    public AdminException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    public AdminException(String mess) {
+        super(mess);
+        // TODO Auto-generated constructor stub
+    }
+
+
 }
