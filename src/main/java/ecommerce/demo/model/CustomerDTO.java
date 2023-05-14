dc51f9aa4de628f4bd997c18ecb4c8910c4aee25
Ramiro <rrcolumbia21@gmail.com>
2023-05-14 12:00:22 +0100

    customer dto

--- src/main/java/ecommerce/demo/model/CustomerDTO.java
+++ src/main/java/ecommerce/demo/model/CustomerDTO.java
@@ -1,4 +1,17 @@
 package ecommerce.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDTO {
+    private Integer id;
+    private String name;
+    private String mobileNumber;
+    private String password;
+    private String email;
+
 }
