abac572c5b05772048a9d75387399af33d06ac80
Ramiro <rrcolumbia21@gmail.com>
2023-05-14 11:56:57 +0100

    address dto

--- src/main/java/ecommerce/demo/model/AddressDto.java
+++ src/main/java/ecommerce/demo/model/AddressDto.java
@@ -1,4 +1,17 @@
 package ecommerce.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressDto {
+
+    private String buildingNo;
+    private String city;
+    private String state;
+    private String country;
+    private String pincode;
 }
