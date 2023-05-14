89b91c9cfe6379071d4c8f8cd0469d3c25622a52
Ramiro <rrcolumbia21@gmail.com>
2023-05-14 11:56:27 +0100

    address

--- src/main/java/ecommerce/demo/model/Address.java
+++ src/main/java/ecommerce/demo/model/Address.java
@@ -1,4 +1,26 @@
 package ecommerce.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  int addressId;
+    private String buildingNo;
+    private String city;
+    private String state;
+    private String country;
+    private String pincode;
+
 }
