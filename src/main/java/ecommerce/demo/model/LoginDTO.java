d634e128080ce872b7c38dbfab5890c6258b9ab4
Ramiro <rrcolumbia21@gmail.com>
2023-05-14 12:01:08 +0100

    login dto

--- src/main/java/ecommerce/demo/model/LoginDTO.java
+++ src/main/java/ecommerce/demo/model/LoginDTO.java
@@ -1,4 +1,14 @@
 package ecommerce.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginDTO {
+
+    private String mobileNumber;
+    private String password;
 }
