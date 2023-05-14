5a997a2e7021689d43ecaa8d603a7daef1be23e3
Ramiro <rrcolumbia21@gmail.com>
2023-05-14 11:53:37 +0100

    order exception

--- src/main/java/ecommerce/demo/exceptions/OrderException.java
+++ src/main/java/ecommerce/demo/exceptions/OrderException.java
@@ -1,4 +1,11 @@
 package ecommerce.demo.exceptions;
 
-public class OrderException {
+public class OrderException  extends Exception{
+
+    public OrderException() {
+
+    }
+    public OrderException(String str) {
+        super(str);
+    }
 }
