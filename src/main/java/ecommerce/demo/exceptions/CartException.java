09e106285e2f96c7518016f75acd702c176f40bb
Ramiro <rrcolumbia21@gmail.com>
2023-05-14 11:48:58 +0100

    cart exception

--- src/main/java/ecommerce/demo/exceptions/CartException.java
+++ src/main/java/ecommerce/demo/exceptions/CartException.java
@@ -1,4 +1,13 @@
 package ecommerce.demo.exceptions;
 
-public class CartException {
+public class CartException extends Exception{
+
+    public CartException() {
+
+    }
+    public CartException(String mess) {
+        super(mess);
+    }
+
+
 }
