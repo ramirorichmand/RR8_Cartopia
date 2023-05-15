00c3efb66d4e9fcfb8dbec0834fd2dc007da911e
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 08:56:52 +0100

    login service

--- src/main/java/ecommerce/demo/service/LoginService.java
+++ src/main/java/ecommerce/demo/service/LoginService.java
@@ -2,5 +2,10 @@ package ecommerce.demo.service;
 
 import ecommerce.demo.exceptions.LoginException;
 import ecommerce.demo.model.LoginDTO;
-public class LoginService {
+public interface LoginService {
+
+    public String logIntoAccount(LoginDTO dto)throws LoginException;
+
+    public String logOutFromAccount(String key)throws LoginException;
+
 }
