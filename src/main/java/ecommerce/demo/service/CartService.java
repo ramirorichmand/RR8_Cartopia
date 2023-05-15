8073e13afefff213d3c9ad034eb1e0e4dd237b32
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 08:53:24 +0100

    cart service

--- src/main/java/ecommerce/demo/service/CartService.java
+++ src/main/java/ecommerce/demo/service/CartService.java
@@ -8,5 +8,16 @@ import ecommerce.demo.model.Cart;
 import ecommerce.demo.model.Product;
 import ecommerce.demo.model.ProductDtoSec;
 
-public class CartService {
+public interface CartService {
+
+    public String addProductToCart(Integer customerId , Integer quantity,Integer productId,String key) throws CustomerException,CartException;
+
+    public List<ProductDtoSec> getAllProduct(String key,Integer CustomerId) throws CustomerException,CartException;
+
+    public String removeProductfromCart(Integer productId,String key,Integer customerId) throws CustomerException,CartException;
+
+    public ProductDtoSec updateQuantity(Integer productId,Integer quantity,String key,Integer customerid) throws CustomerException,CartException;
+
+    public Cart removeAllProduct(String key,Integer customerid) throws CustomerException,CartException;
+
 }
