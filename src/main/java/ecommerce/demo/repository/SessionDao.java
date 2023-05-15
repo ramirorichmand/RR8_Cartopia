6d8d85c3503d0fdfe68caf41121f5ef8c08d511e
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 08:25:48 +0100

    session dao

--- src/main/java/ecommerce/demo/repository/SessionDao.java
+++ src/main/java/ecommerce/demo/repository/SessionDao.java
@@ -1,4 +1,12 @@
 package ecommerce.demo.repository;
 
-public class SessionDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ecommerce.demo.model.CurrentUserSession;
+
+@Repository
+public interface SessionDao extends JpaRepository<CurrentUserSession, Integer> {
+    
+    public  CurrentUserSession  findByUuid(String uuid);
 }
