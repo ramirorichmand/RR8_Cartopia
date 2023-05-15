884a7ba8deffd176060a679213045ebcd883a6b8
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 08:58:03 +0100

    login service impl

--- src/main/java/ecommerce/demo/service/LoginServiceImpl.java
+++ src/main/java/ecommerce/demo/service/LoginServiceImpl.java
@@ -14,5 +14,44 @@ import ecommerce.demo.repository.CustomerDao;
 import ecommerce.demo.repository.SessionDao;
 
 import net.bytebuddy.utility.RandomString;
-public class LoginServiceImpl {
+@Service
+public class LoginServiceImpl implements LoginService{
+
+    @Autowired
+    private CustomerDao cDao;
+
+    @Autowired
+    private SessionDao sDao;
+    
+    @Override
+    public String logIntoAccount(LoginDTO dto)throws LoginException{
+        Customer existingCustomer= cDao.findByMobileNumber(dto.getMobileNumber());
+        if(existingCustomer == null) {
+            throw new LoginException("Please Enter a valid mobile number");
+        }
+        Optional<CurrentUserSession> validCustomerSessionOpt =  sDao.findById(existingCustomer.getCustomerId());
+        if(validCustomerSessionOpt.isPresent()) {
+            throw new LoginException("User already Logged In with this number");
+
+        }
+
+        if(existingCustomer.getPassword().equals(dto.getPassword())) {
+            String key= RandomString.make(6);
+            CurrentUserSession currentUserSession = new CurrentUserSession(existingCustomer.getCustomerId(),key,LocalDateTime.now());
+            sDao.save(currentUserSession);
+            return currentUserSession.toString();
+        }
+        else
+            throw new LoginException("Please Enter a valid password");
+    }
+    @Override
+    public String logOutFromAccount(String key)throws LoginException {
+        CurrentUserSession validCustomerSession = sDao.findByUuid(key);
+        if(validCustomerSession == null) {
+            throw new LoginException("User Not Logged In");
+        }
+        sDao.delete(validCustomerSession);
+        return "Logged Out !";
+    }
+
 }
