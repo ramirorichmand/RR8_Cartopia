e5e08e8863243f599426152f6a390260dd1db518
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 08:55:30 +0100

    customer service

--- src/main/java/ecommerce/demo/service/CustomerService.java
+++ src/main/java/ecommerce/demo/service/CustomerService.java
@@ -5,5 +5,13 @@ import java.util.List;
 import ecommerce.demo.exceptions.CustomerException;
 import ecommerce.demo.model.Customer;
 import ecommerce.demo.model.CustomerDTO;
-public class CustomerService {
+public interface CustomerService {
+
+    public Customer createCustomer(CustomerDTO customer) throws CustomerException;
+
+    public Customer updateCustomer(CustomerDTO customer, String key) throws CustomerException;
+
+    public Customer viewCustomer(Integer customerId, String key, Integer AdminId) throws CustomerException;
+
+    public List<Customer> viewCustomerAll(Integer AdminId, String key) throws CustomerException;
 }
