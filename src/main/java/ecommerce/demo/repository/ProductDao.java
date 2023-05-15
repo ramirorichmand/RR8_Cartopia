615eaa7e4f50ba4ac5935ef27944f45d6f891a73
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 08:24:03 +0100

    product dao

--- src/main/java/ecommerce/demo/repository/ProductDao.java
+++ src/main/java/ecommerce/demo/repository/ProductDao.java
@@ -1,4 +1,9 @@
 package ecommerce.demo.repository;
 
-public class ProductDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ecommerce.demo.model.Product;
+
+public interface ProductDao extends JpaRepository<Product, Integer>{
+
 }
