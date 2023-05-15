e65f06a383fc27bfaacfb33e424663b6f73fd706
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 08:19:06 +0100

    admin dao

--- src/main/java/ecommerce/demo/repository/AdminDao.java
+++ src/main/java/ecommerce/demo/repository/AdminDao.java
@@ -1,4 +1,12 @@
 package ecommerce.demo.repository;
 
-public class AdminDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface AdminDao extends JpaRepository<Admin, Integer>   {
+
+    public Admin findByMobileNumber(String mobileNo);
+
+    public Admin findByAdminId(Integer AdminId);
+
 }
