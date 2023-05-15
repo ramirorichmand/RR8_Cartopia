5b96d03ef181f7d02aaa8921a5dde9f3c968d4f5
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 08:21:59 +0100

    customer dao

--- src/main/java/ecommerce/demo/repository/CartDao.java
+++ src/main/java/ecommerce/demo/repository/CartDao.java
@@ -5,5 +5,5 @@ import org.springframework.data.JpaRepository;
 import ecommerce.demo.model.Cart;
 
 public interface CartDao extends JpaRepository<Cart, Integer> {
-    
+
 }
--- src/main/java/ecommerce/demo/repository/CustomerDao.java
+++ src/main/java/ecommerce/demo/repository/CustomerDao.java
@@ -1,4 +1,11 @@
 package ecommerce.demo.repository;
 
-public class CustomerDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ecommerce.demo.model.Customer;
+
+public interface CustomerDao extends JpaRepository<Customer, Integer>{
+
+    public Customer findByMobileNumber(String mobileNumber);
+
 }
