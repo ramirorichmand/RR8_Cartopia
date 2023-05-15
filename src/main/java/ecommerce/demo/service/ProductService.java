47b96c4e0bd16f4b5b11e23ad18be7e2508fab57
Ramiro <rrcolumbia21@gmail.com>
2023-05-15 09:00:16 +0100

    product srvice

--- src/main/java/ecommerce/demo/service/ProductService.java
+++ src/main/java/ecommerce/demo/service/ProductService.java
@@ -5,5 +5,16 @@ import java.util.List;
 import ecommerce.demo.exceptions.AdminException;
 import ecommerce.demo.model.Product;
 import ecommerce.demo.model.ProductDTO;
-public class ProductService {
+public interface ProductService {
+
+    public Product createProduct(ProductDTO product,String key,Integer adminId) throws AdminException;
+
+    public String removeProduct(Integer productId,String key,Integer adminId) throws AdminException;
+
+    public Product updateProduct(ProductDTO product,String key,Integer adminId) throws AdminException;
+
+    public Product productById(Integer productId,String key,Integer adminId) throws AdminException;
+
+    public List<Product> getAllProduct(String key,Integer adminId) throws AdminException;
+
 }
